5135272df1f5a2616dbe85b22249b06f9ca6ff00
Kairon <jopenski99@gmail.com>
2024-01-18 20:31:41 +0800

    initial push mother fucker

---
+++ src/components/AccountCard.vue
@@ -0,0 +1,36 @@
+<template>
+    <q-card class="justify-center text-center" >
+        <q-card-section>
+            <q-avatar size="150px" font-size="52px" color="teal" text-color="white" icon="directions" />
+        </q-card-section>
+        <q-card-section>
+            <q-input outlined v-model="name.display" dense />
+            <q-input outlined v-model="name.display" dense />
+            <q-input outlined v-model="name.display" dense />
+        </q-card-section>
+    </q-card>
+</template>
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name:'AccountsPage',
+    setup() {
+        const name = {
+            first: null,
+            last: null,
+            middle: null,
+            display: 'Leo Valdez'
+        }
+        const career = {
+            jobTitle : 'Web Developer',
+            level : 'Senior'
+            
+        } 
+
+        return {
+            name
+        }
+    },
+})
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,19 @@
+<template>
+	<q-page class="flex q-pa-md">
+		<div class="row full-width">
+			<div class="col-3 q-ma-sm">
+				<AccountCard />
+			</div>
+		</div>
+	</q-page>
+</template>
+
+<script>
+import AccountCard from "src/components/AccountCard.vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "IndexPage",
+    components: { AccountCard }
+});
+</script>
