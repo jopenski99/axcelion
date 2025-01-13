fc55d1bf41e584a9b25813a9ecebea091d3e4666
John Paul Perez <jopenski99@gmail.com>
2025-01-13 13:04:49 +0800

    Added new components

--- src/pages/PortfolioV2.vue
+++ src/pages/PortfolioV2.vue
@@ -2,91 +2,83 @@
   <div class="row">
     <div class="col-12">
       <div class="row">
-        <div class="col-3">
+        <div class="col-4">
           <q-card class="q-pa-xs q-pl-md full-width row items-center justify-between" style="border-radius: 15px">
             <span style="font-size: 1.1rem">{{ name }}</span>
-            <q-btn
-              round
-              flat
-              dense
-              icon="contrast"
-              @click="$q.dark.toggle()"
-            />
+            <q-btn round flat dense icon="contrast" @click="$q.dark.toggle()" />
           </q-card>
           <q-card class="q-mt-md q-pa-md full-width row items-center justify-between">
-            <div class="col-4">
-              <q-img src="https://cdn.quasar.dev/img/avatar.png" style="width: 90%; height: 90%; border-radius: 10%"/>
+            <div class="col-4" >
+              <q-img src="https://cdn.quasar.dev/img/avatar.png" style="width: 90%; height: 90%; border-radius: 10%" />
             </div>
-           <div class="col-8" style="position: relative">
-             <span style="font-size: 1.1rem; position: absolute; top: 0; right: 0">{{ currentTime }}</span>
-           </div>
+            <div class="col-8 q-pa-xs" style="text-align: right">
+              <span style="font-size: 1.1rem;">{{ currentTime }}</span>
+              <q-card class="q-pa-sm full-width"  style="border: 1px solid #1976d2; box-shadow: none">
+                <div class="row">
+                  <div class="col-3" style="text-align: center" >
+                    <q-avatar class="full-width q-py-md" size="55px" style="height: 100%; display: flex; align-items: center; justify-content: center; border-radius: 10%; border: 1px solid #1976d2;">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" fill="none"
+                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                        class="lucide lucide-github size-10 lg:size-6 xl:size-8">
+                        <path
+                          d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"
+                          stroke="#1976d2">
+                        </path>
+                        <path d="M9 18c-4.51 2-5-2-7-2" stroke="#1976d2"></path>
+                      </svg>
+                    </q-avatar>
+                  </div>
+                  <div class="col-8 q-pl-sm" style="text-align: left">
+                    <div class="text-caption">Latest commit: <a :href="latestCommitUrl" target="_blank" style="color: currentColor">{{ latestCommitHash }}</a></div>
+                    <div class="text-caption text-bold">{{ latestCommitMessage }}</div>
+                    <div>By: <a href="https://github.com/jopenski99" target="_blank" style="color: currentColor">{{lastContributor}}</a> </div>
+                  </div>
+                </div>
+              </q-card>
+            </div>
           </q-card>
         </div>
       </div>
-    <!--   <q-card class="q-pa-md full-width">
-        <q-card-section>
-          <div class="text-h6">Portfolio</div>
-        </q-card-section>
-        <q-card-section>
-          <div class="row">
-            <div class="col-12 col-md-4">
-              <q-img src="https://cdn.quasar.dev/img/avatar.png" style="width: 250px; height: 250px; border-radius: 10%"/>
-            </div>
-            <div class="col-12 col-md-8">
-              <div class="text-subtitle2">John Doe Perez</div>
-              <div class="text-caption">Software Engineer</div>
-              <div class="text-caption">Tools: Vue.js, Node.js, Laravel, AWS, PWA</div>
-              <div class="text-caption">Programming languages: JavaScript, PHP, HTML, CSS</div>
-            </div>
-          </div>
-        </q-card-section>
-        <q-card-section>
-          <div class="text-h6">Work Experience</div>
-        </q-card-section>
-        <q-card-section>
-          <div v-for="xp in workExperience" :key="xp.id">
-            <div class="text-subtitle2">{{xp.company_name}} ({{xp.jobTitle}})</div>
-            <div class="text-caption">{{xp.description}}</div>
-          </div>
-        </q-card-section>
-      </q-card> -->
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'PortfolioV2',
   data() {
     return {
       currentTime: new Date().toLocaleTimeString(),
       name: 'John Doe Perez',
-      workExperience: [
-        {
-          id: 1,
-          company_name: 'Eversun Software Philippines Corp',
-          jobTitle: 'Full Stack Developer',
-          description: 'Worked on 2 projects: an E-learning application that caters educational resources for students, and a Manpower Loaning and Renting Application for both users and administrators.'
-        },
-        {
-          id: 2,
-          company_name: 'Axcelion',
-          jobTitle: 'Software Engineer',
-          description: 'Worked on a project that uses Vue.js and Laravel as our backend, and was responsible for storing and retrieving educational resources, as well as providing a user-friendly interface for users to access the resources.'
-        },
-        {
-          id: 3,
-          company_name: 'Freelance',
-          jobTitle: 'Web Developer',
-          description: 'Worked on a project that uses Vue.js and Laravel as our backend, and was responsible for storing and retrieving educational resources, as well as providing a user-friendly interface for users to access the resources.'
-        }
-      ]
+      latestCommitUrl: '',
+      latestCommitHash: '',
+      latestCommitMessage: '',
+      lastContributor: ''
     }
   },
   mounted() {
     setInterval(() => {
       this.currentTime = new Date().toLocaleTimeString();
     }, 1000);
+    this.getLatestCommit();
+  },
+  methods: {
+    getLatestCommit() {
+      axios.get('https://api.github.com/repos/jopenski99/axcelion/commits')
+        .then(response => {
+          const latestCommit = response.data[0];
+          this.latestCommitUrl = latestCommit.html_url;
+          this.latestCommitHash = latestCommit.sha.substring(0, 7);
+          this.latestCommitMessage = latestCommit.commit.message;
+          this.lastContributor = latestCommit.commit.author.name;
+
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
   }
 }
 </script>
